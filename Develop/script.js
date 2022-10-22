ba0622095079a49671197b11e511f12e28d08191
Jesus M <marquez.jay444@gmail.com>
2022-10-21 19:19:56 -0500

    created acceptance criteria comment and pseudo code in JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,32 @@
+// Assignment Criteria
+  // WHEN user clicks btn, they are given a series of prompts for pwd criteria
+
+  // User selects criteria to include in the password when prompted
+    
+    // Prompted for length( 8-128 chars)
+    // Prompted for char types
+        // chooses to include or not to include:
+              //  lowrCs, UpperCs, numbers, & OR special chars
+
+  // when input answered 
+    // should be validated & atleast 1 chartype should be selected
+
+  // when all prompts answered
+    // password is generaated with selected criteria
+      // displayed in an alert OR writtn on page
+// ------------------------------------------------------------------------------
+
+// Pseudo
+
+// select elemnts from DOM
+
+// create function for generatePassword()
+    // pwd criteria
+    // prompts
+    // conditional statement(s)
+
+// ------------------------------------------------------------------------------
+
 // Assignment code here
 
 
